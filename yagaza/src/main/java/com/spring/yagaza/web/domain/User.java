5923c46be5b5e8d6561b77bb1babe0dab80efd02
yjs2952 <yjs2952@naver.com>
2019-02-20 19:16:05 +0900

    spring validation 적용 중

--- yagaza/src/main/java/com/spring/yagaza/web/domain/User.java
+++ yagaza/src/main/java/com/spring/yagaza/web/domain/User.java
@@ -3,6 +3,10 @@ package com.spring.yagaza.web.domain;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "user")
@@ -21,6 +25,8 @@ public class User {
 	@Column(nullable = false)
 	private String email;
 
+	@NotBlank(message = "아이디를 입력해 주세요.")
+	@Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]{3,}$", message = "영문 숫자 포함 3글자 이상 입력해 주세요.")
 	@Column(nullable = false)
 	private String userId;
 
