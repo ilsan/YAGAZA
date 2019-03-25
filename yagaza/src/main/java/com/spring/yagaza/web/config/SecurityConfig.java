6ae342b4a5137493d84a68bd5bb8d56343177fee
yjs2952 <yjs2952@naver.com>
2019-03-25 21:59:23 +0900

    스프링 시큐리티 적용중

--- yagaza/src/main/java/com/spring/yagaza/web/config/SecurityConfig.java
+++ yagaza/src/main/java/com/spring/yagaza/web/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.spring.yagaza.web.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -12,13 +14,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/resources/**", "/static/**");
-    }
+        web.ignoring()
+                .antMatchers("/resources/**");
+                //.requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+                //.requestMatchers(new AntPathRequestMatcher("/**.html"))
+                //.requestMatchers(new AntPathRequestMatcher("/resources/**"));
+        }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/").permitAll()
+                //.antMatchers("/resources/**, /webjar/**").permitAll()
+                .antMatchers("/", "/resources/**").permitAll()
                 .and()
                 .formLogin()
                     .loginPage("/")
---
+++ yagaza/src/main/java/com/spring/yagaza/web/config/WebMvcConfig.java
@@ -0,0 +1,23 @@
+package com.spring.yagaza.web.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/resources/**");
+               // .addResourceLocations("/resources/");
+                /*.addResourceHandler("/**") //TODO 설정 이렇게 해도 되나요?
+                .addResourceLocations("classpath:/static/")
+                .setCachePeriod(3600)
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver());*/
+    }
+}
